e74c125491ee2f6bbc125c23770dc6b955e9f9e7
Michael Wagner <contact@mwagner.dev>
2022-12-10 09:44:00 +0100

    chore: remove unused code add add features in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { AuthModule } from "./features/auth/auth.module";
+import { ProductModule } from "./features/product/product.module";
 import { UserApiModule } from "./features/user/user-api-module";
+import { VendingMachineModule } from "./features/vending-machine/vending-machine.module";
 
 import { DatabaseModule } from "./shared/database/database.module";
 
 @Module({
-    imports: [DatabaseModule, AuthModule, UserApiModule],
-    controllers: [AppController],
+    imports: [DatabaseModule, AuthModule, UserApiModule, ProductModule, VendingMachineModule],
     providers: [AppService]
 })
 export class AppModule {}
