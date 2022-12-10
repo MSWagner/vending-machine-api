c208e181389baa9b7237e2dee125a98d7814fcca
Michael Wagner <contact@mwagner.dev>
2022-12-10 09:46:18 +0100

    fix: add products controller in the product module

--- src/features/product/product.module.ts
+++ src/features/product/product.module.ts
@@ -5,10 +5,12 @@ import { productProviders } from "./product.providers";
 import { DatabaseModule } from "../../shared/database/database.module";
 import { UserModule } from "../user/user.module";
 import { AuthModule } from "../auth/auth.module";
+import { ProductController } from "./controller/product.controller";
 
 @Module({
     imports: [DatabaseModule, UserModule, AuthModule],
     providers: [...productProviders, ProductService],
+    controllers: [ProductController],
     exports: [DatabaseModule, ProductService, ...productProviders]
 })
 export class ProductModule {}
